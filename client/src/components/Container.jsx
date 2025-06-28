1ff29973d29e33265550d76a9931a985e4ec3338
takeshi0518 <y.takeshi0518@gmail.com>
2025-06-28 12:44:29 +0900

    feat: Containerコンポーネントを作成

---
+++ client/src/components/Container.jsx
@@ -0,0 +1,5 @@
+const Container = ({ children }) => {
+  return <div className="p-6 mx-auto max-w-screen-full">{children}</div>;
+};
+
+export { Container };
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 
 import './tailwind.css';
 import App from './App.jsx';
+import { Container } from './components/Container.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </StrictMode>
 );
