7fcd21dd914cfb395f7fb91a2ba1723b9037f0e5
takeshi0518 <y.takeshi0518@gmail.com>
2025-07-18 14:06:46 +0900

    client: React Routerの導入とNotFooundコンポーネントを作成

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './styles/tailwind.css';
 import { Todo } from './components/Todo.jsx';
+import { NotFound } from './components/NotFound.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Todo />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Todo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
