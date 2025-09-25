630323da002aeaf280ff660cc73322ff700cf456
takeshi0518 <y.takeshi0518@gmail.com>
2025-09-25 16:37:56 +0900

    認証機能実装後に修正予定

--- client/src/components/__tests__/Input.test.jsx
+++ client/src/components/__tests__/Input.test.jsx
@@ -3,29 +3,29 @@ import { render, screen } from '@testing-library/react';
 import { inputs } from '../../styles/uiStyles/inputStyles';
 import { Input } from '../Input';
 
-test('post: タスクが追加されるか', async () => {
-  const addTodo = { _id: '3', content: '追加タスク' };
-  axios.post.mockResolvedValue({ data: addTodo });
-
-  render(
-    <TodoProvider>
-      <TodoForm />
-      <CardList />
-    </TodoProvider>
-  );
-
-  const input = screen.getByPlaceholderText('ToDoを入力してください');
-  const button = screen.getByText('送信');
-
-  await userEvent.type(input, '追加タスク');
-  userEvent.click(button);
-
-  await waitFor(() => {
-    expect(screen.getByText('追加タスク')).toBeInTheDocument();
-  });
-});
-
 describe.skip('Todo機能のテスト（認証機能実装後に修正予定）', () => {
+  test('post: タスクが追加されるか', async () => {
+    const addTodo = { _id: '3', content: '追加タスク' };
+    axios.post.mockResolvedValue({ data: addTodo });
+
+    render(
+      <TodoProvider>
+        <TodoForm />
+        <CardList />
+      </TodoProvider>
+    );
+
+    const input = screen.getByPlaceholderText('ToDoを入力してください');
+    const button = screen.getByText('送信');
+
+    await userEvent.type(input, '追加タスク');
+    userEvent.click(button);
+
+    await waitFor(() => {
+      expect(screen.getByText('追加タスク')).toBeInTheDocument();
+    });
+  });
+
   describe('Inputコンポーネントの正常性確認', () => {
     describe('input要素とtype属性の確認', () => {
       test('input要素が存在し、type属性がtextかどうか', () => {
@@ -35,9 +35,7 @@ describe.skip('Todo機能のテスト（認証機能実装後に修正予定）'
         expect(input).toHaveAttribute('type', 'text');
       });
     });
-  });
 
-  describe.skip('Todo機能のテスト（認証機能実装後に修正予定）', () => {
     describe('variantが正常に渡ってきているかの確認', () => {
       test('variant="large"で正常にclassが登録されているか', () => {
         render(<Input type="text" variant="large" />);
