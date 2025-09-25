b5d5304278c3c470cc38434d96aa0f8c61b643a4
takeshi0518 <y.takeshi0518@gmail.com>
2025-09-25 17:57:21 +0900

    cookieのsameSiteをnoneに設定

--- server/app.mjs
+++ server/app.mjs
@@ -34,7 +34,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       httpOnly: true,
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
